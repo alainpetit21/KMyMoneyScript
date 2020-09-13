ee8d1c5d2b70e13699327195016766f1538d21a3
alainpetit <alainpetit21@hotmail.com>
2020-09-13 09:10:08 -0400

    This file is just to "pseudo-unit-test" my own instruments to make sure that everything works

---
+++ testAllQuotes.py
@@ -0,0 +1,35 @@
+import os
+
+def main():
+    print("0P000072TQ: ")
+    os.system('python3 ./UpdateOnlineQuotes.py ' + "0P000072TQ")
+
+    print("0P0000715V: ")
+    os.system('python3 ./UpdateOnlineQuotes.py ' + "0P0000715V")
+
+    print("0P0000715P: ")
+    os.system('python3 ./UpdateOnlineQuotes.py ' + "0P0000715P")
+
+    print("VUN: ")
+    os.system('python3 ./UpdateOnlineQuotes.py ' + "VUN.TO")
+
+    print("VAB: ")
+    os.system('python3 ./UpdateOnlineQuotes.py ' + "VAB.TO")
+
+    print("TCT-UN: ")
+    os.system('python3 ./UpdateOnlineQuotes.py ' + "TCT-UN.TO")
+
+    print("HQL: ")
+    os.system('python3 ./UpdateOnlineQuotes.py ' + "HQL")
+
+    print("HQH: ")
+    os.system('python3 ./UpdateOnlineQuotes.py ' + "HQH")
+
+    print("XEF: ")
+    os.system('python3 ./UpdateOnlineQuotes.py ' + "XEF.TO")
+
+    print("XEC: ")
+    os.system('python3 ./UpdateOnlineQuotes.py ' + "XEC.TO")
+
+if __name__ == "__main__":
+    main()
